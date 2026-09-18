8e55c7f0ae3df405b10143fbb3b3b48e18aee170
Stepan <edu@sph.py>
2026-09-18 08:24:06 +0000

    file crap added

--- perform.sh
+++ perform.sh
@@ -91,3 +91,11 @@ chmod 660 experiments/tasting_results
 chmod 700 archive_empty
 chmod a-rwx,u+rw,go+r opening_message
 
+cp experiments/tasting_results victor/office/successful_experiment
+cp -r victor/kitchen/molecular_station experiments/molecular_backup
+ln -s ../victor/kitchen/molecular_station/foam_recipe experiments/current_recipe
+ln -s ..victor/kitchen kitchen_entry
+ln opening_message victor/kitchen/shift_order
+cat victor/kitchen/hot_station/senya_report victor/kitchen/hot_station/fedya_report > victor/kitchen/team_report
+cat victor/kitchen/chef_order >> experiments/tasting_results
+mv victor/hall/reservations victor/office/evening_reservations
